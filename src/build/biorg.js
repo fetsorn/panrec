b0aaf36a841f9a1e7b325eb6fc110398935cf352
NasimMora <brs.team.dota@gmail.com>
2023-09-12 17:31:59 +0400

    hardcode Biorg fields

--- src/build/biorg.js
+++ src/build/biorg.js
@@ -2,15 +2,41 @@ import fs from 'fs';
 import path from 'path';
 
 export function buildBiorg(targetPath) {
-  return new WritableStream({
-    objectMode: true,
-    async write(entry, encoding, next) {
-      await fs.promises.appendFile(targetPath, `* .\n${entry.datum}`)
-    },
-    close() {
-    },
-    abort(err) {
-      console.log("Sink error:", err);
-    },
-  });
+	return new WritableStream({
+		objectMode: true,
+
+		async write(entry, encoding, next) {
+			console.log(entry);
+			console.log(`*.
+				:PROPERTIES:
+				:_: ${entry._}
+				:UUID: ${entry.UUID}
+				:DATUM: ${entry.datum}
+				:ACT_DATE: ${entry.actdate}
+				:SAY_DATE: ${entry.saydate}
+				:CATEGORY: ${entry.category}
+				:ACT_NAME: ${entry.actname}
+				:SAY_NAME: ${entry.sayname}
+				:END:
+				* .`);
+		},
+
+		close() {},
+
+		abort(err) {
+			console.log('Sink error:', err);
+		},
+	});
+
+	// return new WritableStream({
+	//   objectMode: true,
+	//   async write(entry, encoding, next) {
+	//     await fs.promises.appendFile(targetPath, `* .\n${entry.datum}`)
+	//   },
+	//   close() {
+	//   },
+	//   abort(err) {
+	//     console.log("Sink error:", err);
+	//   },
+	// });
 }
