0475355cd070abd8971b96df18833aaf12f24fb2
Anton Davydov <fetsorn@gmail.com>
2026-03-20 09:35:03 +0300

    remove renderTitle

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
 import packageJSON from "./package_json.js";
-import renderTitle from "./render_title.js";
 
-export { packageJSON, renderTitle };
+export { packageJSON };
--- src/utils/render_title.js
+++
@@ -1,8 +0,0 @@
-import figlet from "figlet";
-
-export default function renderTitle() {
-  const text = figlet.textSync("My Node.js App", {
-    font: "Small",
-  });
-  console.log(`\n${text}\n`);
-}
